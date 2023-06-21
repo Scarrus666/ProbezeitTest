51828f0e4e76ef9300ec761e3ec03cad5c275364
Scarrus666 <scarrus@gmail.com>
2023-06-21 11:29:43 +0200

    final commit and push

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 function goodSmiley()
     {
-        
+        document.getElementById("smiley").innerHTML = '<div class="col center smiley_good"><p><i class="bi bi-emoji-heart-eyes"></i></p></div>';
+        document.getElementById("under_smiley").innerHTML = '<p class="text_good">Thank you very much, that makes me happy!</p>';
+    }
+
+function badSmiley()
+    {
+        document.getElementById("smiley").innerHTML = '<div class="col center smiley_bad"><p><i class="bi bi-emoji-dizzy"></i></p></div>';
+        document.getElementById("under_smiley").innerHTML = "<p class='text_bad'>Oh no, I'm sorry to hear that!</p>"
     }
